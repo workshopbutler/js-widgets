00d3a5dc701800088335520620b288029c837f6d
Sergey Kotlov <skotlov@gmail.com>
2020-02-01 19:25:32 +0000

    Adds user functions in user templates

--- src/widgets/config/WidgetConfig.ts
+++ src/widgets/config/WidgetConfig.ts
@@ -6,10 +6,16 @@ export default abstract class WidgetConfig {
   readonly fields?: string[];
   readonly theme?: string;
 
+  /**
+   * Functions passed to the widgets
+   */
+  readonly func: IPlainObject;
+
   protected constructor(options: IPlainObject) {
     this.template = options.template ? options.template : undefined;
     this.templateUrl = options.templateUrl ? options.templateUrl : undefined;
     this.fields = options.fields ? options.fields : undefined;
     this.theme = options.theme ? options.theme : undefined;
+    this.func = options.func? options.func : {};
   }
 }
