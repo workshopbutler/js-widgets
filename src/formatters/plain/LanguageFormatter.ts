ca7a3a5bb7ba05cd0ddeada9a8c9579ecc2e1492
egor <egor>
2020-05-26 19:17:11 +0300

    fix

--- src/formatters/plain/LanguageFormatter.ts
+++ src/formatters/plain/LanguageFormatter.ts
@@ -6,9 +6,9 @@ import Localisation from '../../utils/Localisation';
  */
 export default class LanguageFormatter {
 
-  static format(loc: Localisation, language: Language | string | string[]): string | string[]{
+  static format(loc: Localisation, language: Language | string | string[], part: any): string | string[]{
     if (language instanceof Language) {
-      return LanguageFormatter.formatObject(loc, language);
+      return LanguageFormatter.formatObject(loc, language, part);
     }
     if (typeof language === 'string') {
       return loc.translate('language.' + language);
@@ -19,12 +19,21 @@ export default class LanguageFormatter {
     return '';
   }
 
-  protected static formatObject(loc: Localisation, language: Language): string {
-    const prefix = language.spoken.length > 1 ?
-      loc.translate('event.info.twoLangs', { first: language.spoken[0], second: language.spoken[1]}) :
-      loc.translate('event.info.oneLang', { lang: language.spoken[0]}) ;
-    const suffix = language.materials ?
-      loc.translate('event.info.materials', { lang: language.materials }) : '.';
-    return prefix + suffix;
+  protected static formatObject(loc: Localisation, language: Language, part): string {
+    switch(part) {
+      case 'short':
+        return language.spoken.length > 1 ?
+            loc.translate('event.info.shortTwoLangs', {first: language.spoken[0], second: language.spoken[1]}) :
+            loc.translate('language.' + language.spoken[0]);
+
+      default:
+        const
+          prefix = language.spoken.length > 1 ?
+            loc.translate('event.info.twoLangs', {first: language.spoken[0], second: language.spoken[1]}) :
+            loc.translate('event.info.oneLang', {lang: language.spoken[0]});
+        const suffix = language.materials ?
+          loc.translate('event.info.materials', {lang: language.materials}) : '.';
+        return prefix + suffix;
+    }
   }
 }
