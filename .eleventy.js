f6fe34416e2d6d6615dd4ff334358f68f058222f
Max Leonov <eventqueue@gmail.com>
2020-06-10 00:08:20 +0300

    Add layout for preview site

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = function (eleventyConfig) {
-    //eleventyConfig.addPassthroughCopy('src/images')
+    //eleventyConfig.addPassthroughCopy('site/css')
+    //eleventyConfig.addPassthroughCopy('site/js')
 
     return {
         dir: { input: 'el-site', output: 'public', data: '_data' },
         passthroughFileCopy: true,
-        templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+        templateFormats: ['njk', 'md', 'html', 'yml', 'js', 'css'],
         htmlTemplateEngine: 'njk'
     }
 }
