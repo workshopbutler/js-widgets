5f1c99a70af68c63000e952a14eeb8072e7adfe3
Sergey Kotlov <skotlov@gmail.com>
2023-10-19 07:25:44 +0200

    Add seconds to durations for use in promo

--- src/models/utils/Duration.ts
+++ src/models/utils/Duration.ts
@@ -5,10 +5,12 @@ export default class Duration {
   readonly days: number;
   readonly hours: number;
   readonly minutes: number;
+  readonly seconds: number;
 
   constructor(duration: LuxonDuration) {
     this.days = Math.floor(duration.as('days'));
     this.hours = Math.floor(duration.as('hours')) - this.days * 24;
     this.minutes = Math.floor(duration.as('minutes')) - (this.days * 24 + this.hours) * 60;
+    this.seconds = Math.floor(duration.as('seconds')) - ((this.days * 24 + this.hours) * 60 + this.minutes) * 60;
   }
 }
